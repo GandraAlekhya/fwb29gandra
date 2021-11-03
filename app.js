156babe9ce2ffdff04945cd51b67700a354cbac4
Alekhya Gandra <alekhya.rachu@gmail.com>
2021-11-03 02:15:30 -0500

    final push

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var batmanRouter = require('./routes/batman');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var addmodsRouter = require('./routes/addmods');
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -20,7 +22,10 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
+app.use('/batman', batmanRouter);
 app.use('/users', usersRouter);
+app.use('/selector', selectorRouter);
+app.use('/addmods', addmodsRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
